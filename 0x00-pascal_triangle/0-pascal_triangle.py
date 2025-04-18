6534662ec8a3c04bf82479f9bbd19458b35d28b0
makau6 <makauwanyoike@gmail.com>
2025-04-18 20:29:00 +0300

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,9 +13,9 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = []
-    
+
     for i in range(n):
         # Start a new row with 1
         row = [1]
@@ -26,5 +26,5 @@ def pascal_triangle(n):
                 row.append(last_row[j - 1] + last_row[j])
             row.append(1)  # End the row with 1
         triangle.append(row)
-    
+
     return triangle
