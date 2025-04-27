3a0b19dd39bbc8e601122af3cea9a00a8690680e
makau6 <makauwanyoike@gmail.com>
2025-04-27 10:42:01 +0300

    locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be unlocked
+    Args:
+    boxes: A list of lists; inner lists contain keys to other boxes (lists)
+    Returns:
+    bool: True if all boxes can be unlocked, False otherwise
+    """
+    if not boxes:
+        return False
+
+    unlocked = [0]
+
+    for i in unlocked:
+        for key in boxes[i]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    return len(unlocked) == len(boxes)
