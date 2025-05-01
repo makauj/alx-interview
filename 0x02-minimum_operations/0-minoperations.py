b12aa8ae481116848614610e008528165a0c9c3a
makau6 <makauwanyoike@gmail.com>
2025-05-01 19:59:29 +0300

    2nd try

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -18,9 +18,8 @@ def minOpereations(n):
     divisor = 2
 
     while n > 1:
-        if n % divisor == 0:
+        while n % divisor == 0:
             operations += divisor
             n //= divisor
-        else:
-            divisor += 1
+        divisor += 1
     return operations
