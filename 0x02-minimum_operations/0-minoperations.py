4d7f65d612f7d97cbe04e98d65ab518e27dfb93c
makau6 <makauwanyoike@gmail.com>
2025-05-01 19:40:49 +0300

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def minOpereations(n):
+    """
+    Script that only does two commands: copy all and paste all
+    args:
+        n: an integer. If n cannot be acheived, return 0
+    Return:
+        the minimum number of operations needed to get n H characters
+        0 if n is not possible
+    """
+    if n < 2:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+    return operations
