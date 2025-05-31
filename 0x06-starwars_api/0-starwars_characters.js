cd470220a69467a385a026b7d9a203fe85f30b19
makau6 <makauwanyoike@gmail.com>
2025-05-31 13:39:48 +0300

    starwars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// This script sets up a simple Express server that fetches
+// data from the ALX Starwars API
+
+const request = require('request');
+const movieId = process.argv[2];
+const mEndPoint = 'https://swap-api.alx-tools.com/api/films/' + movieId;
+
+function getRequest(characterList, index) {
+  if (index >= characterList.length) {
+    return;
+  }
+  
+  request(characterList[index], (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const character = JSON.parse(body);
+      console.log(character.name);
+    } else {
+      console.error('Error fetching character:', error);
+    }
+    getRequest(characterList, index + 1);
+  });
+}
+
+request(mEndPoint, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    const characterList = movie.characters;
+    getRequest(characterList, 0);
+  } else {
+    console.error('Error fetching movie:', error);
+  }
+});
